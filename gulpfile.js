7b5a4ebaf88820d34c9388868de26ffd5079eb57
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-12-08 22:34:27 -0200

    fix indent issues

--- gulpfile.js
+++ gulpfile.js
@@ -8,24 +8,24 @@ var gulp 		= require('gulp'), // responsavel pelo processo de build
 	notifier	= require('node-notifier');
 
 gulp.task('browser-sync', function () {
-   var files = [
-      'app/**/*.html'
-      //'app/src/css/**/*.css',
-      //'app/src/img/**/*',
-      //'app/src/js/**/*.js',
-      //'app/src/jsx/**/*.jsx'
-   ];
+	var files = [
+		'app/**/*.html'
+		//'app/src/css/**/*.css',
+		//'app/src/img/**/*',
+		//'app/src/js/**/*.js',
+		//'app/src/jsx/**/*.jsx'
+	];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: 'build/'
-      }
-   });
+	browserSync.init(files, {
+		server: {
+			baseDir: 'build/'
+		}
+	});
 });
 
 
 gulp.task('react', function () {
-    var bundler = watchify(browserify({
+	var bundler = watchify(browserify({
 		entries: ['./app/src/jsx/app.jsx'], 
 		transform: [reactify],
 		extensions: ['.jsx'],
@@ -34,26 +34,26 @@ gulp.task('react', function () {
 		packageCache: {},
 		fullPaths: true
 	}, watchify.args))
-        .on('update', function () { gutil.log('Rebundling...'); })
-        .on('time', function (time) {
-            gutil.log('Rebundled in:', gutil.colors.cyan(time + 'ms'));
-        });
+		.on('update', function () { gutil.log('Rebundling...'); })
+		.on('time', function (time) {
+			gutil.log('Rebundled in:', gutil.colors.cyan(time + 'ms'));
+		});
 
-    bundler.transform(reactify);
-    bundler.on('update', rebundle);
+	bundler.transform(reactify);
+	bundler.on('update', rebundle);
 
-    function rebundle() {
-        return bundler.bundle()
-            .on('error', function (err) {
-                gutil.log(err);
-                notifier.notify({ title: 'Browserify Error', message: 'Something went wrong :/' });
-            })
-        	.pipe(source('components.js'))
-        	.pipe(gulp.dest('./build/js'))
-        	.pipe(browserSync.reload({ stream: true }));
-    }
+	function rebundle() {
+		return bundler.bundle()
+			.on('error', function (err) {
+				gutil.log(err);
+				notifier.notify({ title: 'Browserify Error', message: 'Something went wrong :/' });
+			})
+			.pipe(source('components.js'))
+			.pipe(gulp.dest('./build/js'))
+			.pipe(browserSync.reload({ stream: true }));
+	}
 
-    return rebundle();
+	return rebundle();
 });
 
 gulp.task('html', function () {
@@ -62,12 +62,12 @@ gulp.task('html', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['app/**/*.html'], ['html']);
-  //gulp.watch('app/src/js/*.js', ['scripts']);
-  //gulp.watch('app/src/jsx/*.jsx', ['jsx']);
-  //gulp.watch('app/src/css/*.css', ['css']);
-  //gulp.watch('app/src/styl/*.styl', ['stylus']);
-  //gulp.watch('app/src/fonts/**/*', ['fonts']);
+	gulp.watch(['app/**/*.html'], ['html']);
+	//gulp.watch('app/src/js/*.js', ['scripts']);
+	//gulp.watch('app/src/jsx/*.jsx', ['jsx']);
+	//gulp.watch('app/src/css/*.css', ['css']);
+	//gulp.watch('app/src/styl/*.styl', ['stylus']);
+	//gulp.watch('app/src/fonts/**/*', ['fonts']);
 });
 
 gulp.task('default', ['html', 'react', 'watch','browser-sync']);
